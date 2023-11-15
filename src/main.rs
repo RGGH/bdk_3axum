fb116f3df33890b2ff842ff088bdbefe802b8918
RGGH <pisscompany@gmail.com>
2023-11-15 20:05:35 +0000

    add route compostion

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::net::SocketAddr;
 use axum::extract::Query;
 
+use axum::extract::Path;
 use axum::Router;
 use axum::routing::get;
 use axum::response::{Html, IntoResponse};
@@ -14,23 +15,31 @@ struct HelloParams {
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route(
-        "/hello"
-        ,get(handler_hello));
+    let routes_all = Router::new().merge(routes_hello());
 
     let addr = SocketAddr::from(([127,0,0,1],8080));
     println!("->> LISTENING ON {}", &addr);
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
 
-//
+// - Routes Hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello",get(handler_hello))
+        .route("/hello2/:name",get(handler_hello2))
+}
 // eg /hello?name=bob
 async fn handler_hello(Query(params):Query<HelloParams>)->impl IntoResponse{
     println!("->>{:<12}","Handler");
     let name = params.name.as_deref().unwrap_or("world"); 
     Html(format!("<strong>{name}!</strong>"))
+}
+
+async fn handler_hello2(Path(name) : Path<String>) -> impl IntoResponse {
+    println!("->>{:<12}","Handler");
+    Html(format!("<strong>{name}!</strong>"))
+
 }
