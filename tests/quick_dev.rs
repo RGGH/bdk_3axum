0113ff40fe7cf3d3ab36c19f25716eed282cd3a2
RGGH <pisscompany@gmail.com>
2023-11-16 13:55:34 +0000

    pre

--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -4,6 +4,7 @@ use anyhow::Result;
 #[tokio::test]
 async fn quick_dev() -> Result<()>{
     let hc = httpc_test::new_client("http://localhost:8080")?;
-    hc.do_get("/hello?name=bob").await?.print().await?;
+    //hc.do_get("/hello?name=bob").await?.print().await?;
+    hc.do_get("/hello2/xob").await?.print().await?;
     Ok(())
 }
